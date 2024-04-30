a82828fa08cce3d64bcd0ebc77618f53743904b7
alriansyah <alriansyah1218@gmail.com>
2024-05-01 02:54:29 +0700

    feat: Learn nest cookie-parser

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,91 +1,103 @@
 import {
-    Controller,
-    Get,
-    Post,
-    Put,
-    Param,
-    Query,
-    Res,
-    Header,
-    HttpCode,
-    HttpRedirectResponse,
-    Redirect,
+  Controller,
+  Get,
+  Post,
+  Put,
+  Param,
+  Query,
+  Res,
+  Req,
+  Header,
+  HttpCode,
+  HttpRedirectResponse,
+  Redirect,
 } from '@nestjs/common';
 import { Response, Request } from 'express';
 
 @Controller('/api/user')
 export class UserController {
-    @Post()
-    post(): string {
-        return 'post user';
+  @Get('/set-cookie')
+  setCookie(@Query('name') name: string, @Res() response: Response) {
+    response.cookie('name', name);
+    response.status(200).send('Success set cookie');
+  }
+
+  @Get('/get-cookie')
+  getCookie(@Req() req: Request) {
+    return req.cookies['name'];
+  }
+
+  @Post()
+  post(): string {
+    return 'post user';
+  }
+
+  // @Get('/sample-response')
+  // sampleResponse(@Res() res: Response) { // express http response
+  //     res.status(200).json({
+  //         data: 'Hello World!',
+  //     });
+  // }
+
+  @Get('/sample-response')
+  @Header('Content-Type', 'application/json')
+  @HttpCode(200)
+  sampleResponse(): Record<string, string> {
+    // express http response
+    return {
+      data: 'Hello World!',
+    };
+  }
+
+  @Get('/redirect')
+  @Redirect()
+  redirect(): HttpRedirectResponse {
+    return { url: '/api/user/sample-response', statusCode: 301 };
+  }
+
+  // @Get('/detail/:id')
+  // getDeatailUser(@Req() req: Request): string { // menggunakan express request
+  //     return `get user ${req.params.id}`;
+  // }
+
+  @Get('/detail/:id')
+  getUserParam(@Param('id') id: string): string {
+    // menggunakan decorator
+    return `Param: ${id}`;
+  }
+
+  // ex: api/user/query?names=Al
+  @Get('/query')
+  getUserQuery(@Query('names') names: string): string {
+    return `Query: ${names || 'Guest'} `;
+  }
+
+  // ex: api/user/multi-query?first_name=Al&last_name=Riansyah
+  @Get('/multi-query')
+  getMultiQuery(
+    @Query('first_name') firstName: string,
+    @Query('last_name') lastName: string,
+  ): string {
+    return `Query: ${firstName || ''} ${lastName || ''}`;
+  }
+
+  @Get('/sample')
+  get(): string {
+    return 'get user sample';
+  }
+
+  // Support async
+  @Get('/async')
+  async getAsync(): Promise<string> {
+    try {
+      return 'get user async';
+    } catch (error) {
+      return error;
     }
+  }
 
-    // @Get('/sample-response')
-    // sampleResponse(@Res() res: Response) { // express http response
-    //     res.status(200).json({
-    //         data: 'Hello World!',
-    //     });
-    // }
-
-    @Get('/sample-response')
-    @Header('Content-Type', 'application/json')
-    @HttpCode(200)
-    sampleResponse(): Record<string, string> {
-        // express http response
-        return {
-            data: 'Hello World!',
-        };
-    }
-
-    @Get('/redirect')
-    @Redirect()
-    redirect(): HttpRedirectResponse {
-        return { url: '/api/user/sample-response', statusCode: 301 };
-    }
-
-    // @Get('/detail/:id')
-    // getDeatailUser(@Req() req: Request): string { // menggunakan express request
-    //     return `get user ${req.params.id}`;
-    // }
-
-    @Get('/detail/:id')
-    getUserParam(@Param('id') id: string): string {
-        // menggunakan decorator
-        return `Param: ${id}`;
-    }
-
-    // ex: api/user/query?names=Al
-    @Get('/query')
-    getUserQuery(@Query('names') names: string): string {
-        return `Query: ${names || 'Guest'} `;
-    }
-
-    // ex: api/user/multi-query?first_name=Al&last_name=Riansyah
-    @Get('/multi-query')
-    getMultiQuery(
-        @Query('first_name') firstName: string,
-        @Query('last_name') lastName: string,
-    ): string {
-        return `Query: ${firstName || ''} ${lastName || ''}`;
-    }
-
-    @Get('/sample')
-    get(): string {
-        return 'get user sample';
-    }
-
-    // Support async
-    @Get('/async')
-    async getAsync(): Promise<string> {
-        try {
-            return 'get user async';
-        } catch (error) {
-            return error;
-        }
-    }
-
-    @Put()
-    put(): string {
-        return 'put user';
-    }
+  @Put()
+  put(): string {
+    return 'put user';
+  }
 }
